33c442b460bcfdb9af5f57b84c4daf0ac3ee53a7
babooc7579-creator <babooc7579@gmail.com>
2026-06-27 14:06:10 +0900

    refactor: add video sort constants

---
+++ src/constants/sortOptions.js
@@ -0,0 +1,13 @@
+export const VIDEO_SORT_OPTIONS = [
+  { value: 'multiplier', label: '성과 배수순' },
+  { value: 'viral', label: '일 조회수순' },
+  { value: 'views', label: '조회수순' },
+  { value: 'likes', label: '좋아요 비율순' },
+  { value: 'date', label: '최신순' },
+];
+
+export const VIDEO_LENGTH_FILTERS = [
+  { value: 'all', label: '전체' },
+  { value: 'shorts', label: '쇼츠' },
+  { value: 'long', label: '롱폼' },
+];
