969d1e8133b3a4489fae5e30d02105bfbd8f6967
babooc7579-creator <babooc7579@gmail.com>
2026-06-27 13:47:33 +0900

    refactor: add app config file

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// App-wide configuration values.
+// Keep API keys and secrets out of this file.
+
+export const FUNCTION_API_BASE = 'https://yt-analyzer-func-hyd8hxbwb8gkephg.koreacentral-01.azurewebsites.net/api';
+
+export const YOUTUBE_API_BASE = 'https://www.googleapis.com/youtube/v3';
