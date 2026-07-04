63acf29a0230e314a9a91eb08363433f35b0c7ab
화용 <babooc7579@gmail.com>
2026-07-05 03:44:01 +0900

    refactor: add route props builder

--- src/utils/appRouteProps.js
+++ src/utils/appRouteProps.js
@@ -298,3 +298,25 @@ export function buildLegacyWorkspaceRouteProps({
     visibleScrapCount,
   };
 }
+
+export function buildRoutesProps({
+  activeCreatorItem,
+  discoveryLinksRouteProps,
+  homeRouteProps,
+  isComingSoonView,
+  isDiscoveryLinksView,
+  isHomeView,
+  isLegacyWorkspaceView,
+  legacyWorkspaceRouteProps,
+}) {
+  return {
+    activeCreatorItem,
+    discoveryLinksRouteProps,
+    homeRouteProps,
+    isComingSoonView,
+    isDiscoveryLinksView,
+    isHomeView,
+    isLegacyWorkspaceView,
+    legacyWorkspaceRouteProps,
+  };
+}
