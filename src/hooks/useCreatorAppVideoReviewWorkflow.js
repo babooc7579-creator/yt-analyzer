2369da9bd067a2203089760210d605bf764d2da1
babooc7579-creator <babooc7579@gmail.com>
2026-07-07 18:13:31 +0900

    refactor: extract video review workflow hook

---
+++ src/hooks/useCreatorAppVideoReviewWorkflow.js
@@ -0,0 +1,44 @@
+import { useScrapbook } from './useScrapbook';
+import { useVideoProductionActions } from './useVideoProductionActions';
+import { useVideoUserRecords } from './useVideoUserRecords';
+
+export function useCreatorAppVideoReviewWorkflow() {
+  const {
+    savedVideos,
+    scrapbookSyncWarning,
+    isVideoSaved,
+    toggleScrapVideo,
+  } = useScrapbook();
+  const {
+    videoUserRecords,
+    videoRecordsSyncWarning,
+    markVideoStatus: markRadarVideoStatus,
+    updateVideoUserRecord,
+    restoreVideoToRadar,
+    clearRadarDecisions,
+  } = useVideoUserRecords();
+  const {
+    isProductionCandidate,
+    promoteVideoToProduction,
+  } = useVideoProductionActions({
+    isVideoSaved,
+    markVideoStatus: markRadarVideoStatus,
+    toggleScrapVideo,
+    videoUserRecords,
+  });
+
+  return {
+    clearRadarDecisions,
+    isProductionCandidate,
+    isVideoSaved,
+    markRadarVideoStatus,
+    promoteVideoToProduction,
+    restoreVideoToRadar,
+    savedVideos,
+    scrapbookSyncWarning,
+    toggleScrapVideo,
+    updateVideoUserRecord,
+    videoRecordsSyncWarning,
+    videoUserRecords,
+  };
+}
